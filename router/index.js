a5b6fb7f67c9c87bb2903e6089f376f8f73fcbf4
Cristian Aldair Bonifacio Quispe <u20201b845@upc.edu.pe>
2023-12-01 11:33:15 -0500

    Commit
    Add UpdateByCasoIdByUserId.
    Falta testear el archivo actualizado. da error

--- router/index.js
+++ router/index.js
@@ -1,26 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const CasosController = require('../Controllers/CasosController.js');
-//<<<<<<< HEAD
 const DocClienteController = require('../Controllers/DocClienteController.js');
-//module.exports = function () {}
 const PagosController = require('../Controllers/PagosController.js');
-//=======
 const UsuarioController = require('../Controllers/UsuarioController.js');
-module.exports = function () {
 const NoticiasController = require('../Controllers/NoticiasController.js');
 
-    /** PAGOS */
+module.exports = function () {
+
+
+ /** PAGOS */
+    //Agregar pagos
     router.post('/pagos',
         PagosController.CrearPagos
     );
+    //Obtener pagos
     router.get('/pagos',
         PagosController.obtenerPagos
     );
-    /**Noticias */
-    router.post('/noticias',
-        NoticiasController.crearNoticias
+
+    //Metodo par Mostrar Pagos
+    router.get('/mostrar-pagos',
+    PagosController.mostrarPagos
     );
+
+// Actualizar Pagos
+router.put('/mostrar-pagos/:idPagos',
+    PagosController.actualizarPago,
+    PagosController.mostrarPagos
+
+    );
+// Mostrar Pagos por ID
+router.get('/pagos/:idPagos',
+    PagosController.mostrarPagosID
+);
+
+// Eliminar Pagos
+router.delete('/pagos/eliminar/:idPagos',
+    PagosController.eliminarPagos
+);
+
+  /**Noticias */
+  router.post('/noticias',
+  NoticiasController.crearNoticias
+);
  
 
     /** USUARIO*/
@@ -47,7 +70,7 @@ const NoticiasController = require('../Controllers/NoticiasController.js');
 
 
     /** CASOS */
-//>>>>>>> main
+
 
     //Metodo par Mostrar Casos
     router.get('/trabajador-casos',
@@ -72,17 +95,18 @@ const NoticiasController = require('../Controllers/NoticiasController.js');
     
         CasosController.encontrarCasosByUser
     );
+
+    //Actualizar Casos ByUserId and CasoId
+    router.put('/usuarios/:userid/casos/:idCasos',
+    CasosController.subirArchivo,
+    CasosController.actualizarCasoIdByUser);
+
     // Actualizar Casos
     router.put('/trabajador-casos/:idCasos',
 
         CasosController.subirArchivo,
         CasosController.actualizarCaso
     )
-    
-//<<<<<<< HEAD
-
-
-
 
     /** DOCUMENTO CLIENTE  */
 
@@ -90,13 +114,13 @@ const NoticiasController = require('../Controllers/NoticiasController.js');
     router.post('/subir-documentos', 
     DocClienteController.agregardoccliente
     );
-//=======
+
     // Eliminar Casos
     router.delete('/trabajador-casos/:idCasos',
     
         CasosController.eliminarCasos
     )
-//>>>>>>> main
+
         
     
 
