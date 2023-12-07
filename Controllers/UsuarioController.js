506356bda3e4779147bf28f91e52f30a34789118
Cristian Aldair Bonifacio Quispe <u20201b845@upc.edu.pe>
2023-12-07 00:55:31 -0500

    Commit Views Roles
    Test Views by Roles

---
+++ Controllers/UsuarioController.js
@@ -0,0 +1,135 @@
+const Usuario = require('../Models/Usuario');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+exports.formIniciarSesion = (req,res) =>{
+    res.render('iniciar-sesion',{
+        isHome: false
+    });
+}
+exports.crearUsuario = async (req, res, next) => {
+    const usuarios = new Usuario(req.body);
+    try {
+        //almacenar un registro
+        await usuarios.save();
+        res.json({mensaje: 'Se agrego un nuevo usuario'});
+    } catch (error) {
+        //si hay un error
+        res.send(error);
+        next();
+    }
+};
+
+
+exports.obtenerUsuarios = async (req, res) => {
+    try {
+        const usuarios = await Usuario.findAll();
+        res.send(usuarios);
+    } catch (error) {
+        res.status(500).send('Hubo un error');
+    }
+};
+
+// Mostrar Usuario por ID
+exports.mostrarUsuarioID = async(req,res,next)=>{
+    const usuarios = await Usuario.findByPk(req.params.idUsu);
+
+     if(!usuarios) {
+         res.json({mensaje : 'Ese Usuario no existe'});
+         return next();
+     }
+
+     // Mostrar  el caso
+     res.json(usuarios);
+ }
+
+ exports.actualizarUsuario = async (req, res) => {
+    try {
+        let usuario = await Usuario.findByPk(req.params.idUsu);
+
+        if (!usuario) {
+            res.status(404).send('Usuario no encontrado');
+            return;
+        }
+
+        // Construir un objeto con los campos a actualizar
+        const updateObj = {
+            email: req.body.email,
+        };
+
+        // Verificar si la contraseña se proporciona y cifrarla
+        if (req.body.password) {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            updateObj.password = hashedPassword;
+        }
+
+        // Actualizar el usuario en la base de datos
+        await Usuario.update(updateObj, {
+            where: { id: req.params.idUsu }
+        });
+
+        // Recuperar el usuario actualizado
+        usuario = await Usuario.findByPk(req.params.idUsu);
+
+        res.send(usuario);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Hubo un error');
+    }
+};
+
+exports.eliminarUsuario = async (req, res) => {
+    try {
+        let usuario = await Usuario.findByPk(req.params.idUsu);
+        if (!usuario) {
+            res.status(404).send('Usuario no encontrado');
+            return;
+        }
+        await Usuario.destroy({
+            where: { id: req.params.idUsu }
+        });
+        res.send({ message: 'Usuario eliminado' });
+    } catch (error) {
+        res.status(500).send('Hubo un error');
+    }
+};
+
+// exports.autenticarUsuario = async (req, res, next) => {
+//     try {
+//         // Buscar el usuario
+//         const { email, password } = req.body;
+//         const usuario = await Usuario.findOne({ where: { email } });
+
+//         if (!usuario) {
+//             // Si el usuario no existe
+//             return res.status(401).json({ mensaje: 'Ese usuario no existe' });
+//         }
+
+//         // Verificar si la contraseña es correcta
+//         const contrasenaCorrecta = await bcrypt.compare(password, usuario.password);
+
+//         if (!contrasenaCorrecta) {
+//             // Si la contraseña es incorrecta
+//             return res.status(401).json({ mensaje: 'Contraseña incorrecta' });
+//         }
+
+//         // Contraseña correcta, firma el token
+//         const token = jwt.sign(
+//             {
+//                 email: usuario.email,
+//                 usuario: usuario.nombre,
+//                 _id: usuario.id,
+//             },
+//             process.env.JWT_SECRET || 'LLAVESECRETA',
+//             {
+//                 expiresIn: '1h',
+//             }
+//         );
+
+//         // Retornar el TOKEN
+//         return res.json({ token });
+//     } catch (error) {
+//         console.error('Error en autenticarUsuario:', error);
+//         return res.status(500).json({ mensaje: 'Error interno del servidor' });
+//     }
+// };
--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,8 +1,32 @@
 const passport = require('passport');
+const Usuario = require('../Models/Usuario'); // Asegúrate de importar correctamente el modelo de Usuario
 
-exports.autenticarUsuario = passport.authenticate('local',{
-    successRedirect : '/',
-    failureRedirect : '/iniciar-sesion',
-    failureFlash : true,
-    badRequestMessage : 'Ambos campos son obligatorios'
-})
+exports.autenticarUsuario = (req, res, next) => {
+    passport.authenticate('local', (err, usuario, info) => {
+        if (err) {
+            return next(err);
+        }
+
+        if (!usuario) {
+            // Puedes manejar la lógica de redirección en caso de falla aquí
+            return res.redirect('/iniciar-sesion');
+        }
+
+        // Define las rutas según el rol del usuario
+        let redirectPath = '/';
+        if (usuario.role === 'admin') {
+            redirectPath = '/admin/home';
+        } else if (usuario.role === 'trabajador') {
+            redirectPath = '/trabajador/home';
+        } else if (usuario.role === 'cliente') {
+            redirectPath = '/cliente/home';
+        }
+
+        req.logIn(usuario, (err) => {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect(redirectPath);
+        });
+    })(req, res, next);
+};
